1ce8eb704e6a2c5b384c79379fe20a5f9acde444
kuueli <kuu@MacBook-Pro-de-kuueli.local>
2021-11-17 13:23:55 +0100

    random words from dictionnary

--- hangman/src/App.js
+++ hangman/src/App.js
@@ -1,22 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Axios from 'axios';
+import { FaSearch } from 'react-icons/fa';
+import { FcSpeaker } from 'react-icons/fc';
+import words from './randomWord.js'
 
 function App() {
+
+  const [data, setData] = useState('');
+  const [searchWord, setSearchWord] = useState('');
+ 
+  const handleClick = () => {
+    var item = words[Math.floor(Math.random() * words.length)];
+    setSearchWord(item);
+    }
+  function getMeaning() {
+    Axios.get('https://api.dictionaryapi.dev/api/v2/entries/en_US/${searchWord}').then((response) => {
+      setData(response.data[0]);
+    });
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button value="random words" onClick={handleClick}>random words</button>
+        <div>
+          <p>{searchWord}</p>
+        </div>
       </header>
     </div>
   );
